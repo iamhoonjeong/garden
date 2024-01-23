391db65f3c5a2e3e2776675efb370f32b54fc2ee
Hoon Jeong <iamhoonjeong@gmail.com>
2024-01-23 23:57:26 +0900

    css change

--- app/hand/page.tsx
+++ app/hand/page.tsx
@@ -43,8 +43,8 @@ export default function Hand() {
 
       video.style.minWidth = `100vw`;
       video.style.minHeight = `100vh`;
-      // video.style.maxWidth = `100vw`;
-      // video.style.maxHeight = `100vh`;
+      video.style.maxWidth = `100vw`;
+      video.style.maxHeight = `100vh`;
     }
 
     videoPlay(video, videoConstraints);
