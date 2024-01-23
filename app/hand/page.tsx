b02ecf871f4a07be4ecacaee2d4cfefbc3fa94b9
Hoon Jeong <iamhoonjeong@gmail.com>
2024-01-23 23:48:01 +0900

    using viewport units

--- app/hand/page.tsx
+++ app/hand/page.tsx
@@ -41,8 +41,10 @@ export default function Hand() {
         },
       };
 
-      video.style.minWidth = `100%`;
-      video.style.maxHeight = `${screen.height}px`;
+      video.style.minWidth = `100vw`;
+      video.style.minHeight = `100vh`;
+      video.style.maxWidth = `100vw`;
+      video.style.maxHeight = `100vh`;
     }
 
     videoPlay(video, videoConstraints);
